b6dc680b9fda775bb5cbff8e697355617f835bc6
Takatoshi Matsumoto <toqoz403@gmail.com>
2015-11-12 02:15:59 +0900

    Define build tasks for watch and normal version

--- gulpfile.js
+++ gulpfile.js
@@ -4,33 +4,44 @@ var mustache = require("gulp-mustache-plus");
 var webserver = require('gulp-webserver');
 var sass = require('gulp-sass');
 
-gulp.task('semantic', function() {
-    gulp.src('semantic/dist/*.js').pipe(gulp.dest('public/javascripts'));
-    gulp.src('semantic/dist/*.css').pipe(gulp.dest('public/stylesheets'));
-    gulp.src('semantic/dist/themes/**').pipe(gulp.dest('public/stylesheets/themes'));
-    gulp.src('semantic/dist/*.js').pipe(gulp.dest('public/javascripts'));
-});
+var sources = {
+    s: function s(path, options) {
+        return gulp.src(path, options);
+    },
+    w: function w(path, options) {
+        return gulp.src(path, options).pipe(watch(path, options));
+    }
+};
 
-gulp.task('templates', function() {
-    w("_templates/**/index.tmpl", {base: './_templates'})
-        .pipe(mustache({},{},{
-            head:   "_templates/partial/head.tmpl",
-            nav: "_templates/partial/nav.tmpl",
-        })).pipe(gulp.dest("public"));
-});
+['s', 'w'].forEach(function(prefix) {
+    gulp.task(prefix + 'semantic', function() {
+        gulp.src('semantic/dist/*.js').pipe(gulp.dest('public/javascripts'));
+        gulp.src('semantic/dist/*.css').pipe(gulp.dest('public/stylesheets'));
+        gulp.src('semantic/dist/themes/**').pipe(gulp.dest('public/stylesheets/themes'));
+        gulp.src('semantic/dist/*.js').pipe(gulp.dest('public/javascripts'));
+    });
 
-gulp.task('stylesheets', function() {
-    w('_stylesheets/*.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('public/stylesheets'));
-});
+    gulp.task(prefix + 'templates', function() {
+        sources[prefix]("_templates/**/index.tmpl", {base: './_templates'})
+            .pipe(mustache({},{},{
+                head:   "_templates/partial/head.tmpl",
+                nav: "_templates/partial/nav.tmpl",
+            })).pipe(gulp.dest("public"));
+    });
 
-gulp.task('javascripts', function() {
-    w('_javascripts/*.js').pipe(gulp.dest('public/javascripts'));
-});
+    gulp.task(prefix + 'stylesheets', function() {
+        sources[prefix]('_stylesheets/*.scss')
+            .pipe(sass().on('error', sass.logError))
+            .pipe(gulp.dest('public/stylesheets'));
+    });
 
-gulp.task('images', function() {
-    w('_images/**/*', {base: './_images'}).pipe(gulp.dest('public/images'));
+    gulp.task(prefix + 'javascripts', function() {
+        sources[prefix]('_javascripts/*.js').pipe(gulp.dest('public/javascripts'));
+    });
+
+    gulp.task(prefix + 'images', function() {
+        sources[prefix]('_images/**/*', {base: './_images'}).pipe(gulp.dest('public/images'));
+    });
 });
 
 gulp.task('serve', function() {
@@ -44,10 +55,8 @@ gulp.task('serve', function() {
     }, 1000);
 });
 
-gulp.task('build', ['semantic', 'templates', 'stylesheets', 'javascripts', 'images']);
-gulp.task('dev', ['build', 'serve']);
-gulp.task('release', ['build']);
+gulp.task('build', ['ssemantic', 'stemplates', 'sstylesheets', 'sjavascripts', 'simages']);
+gulp.task('watch-build', ['wsemantic', 'wtemplates', 'wstylesheets', 'wjavascripts', 'wimages']);
 
-function w(path, options) {
-    return gulp.src(path, options).pipe(watch(path, options));
-}
+gulp.task('release', ['build']);
+gulp.task('dev', ['watch-build', 'serve']);
